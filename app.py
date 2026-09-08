5e74027e24f66432915f1c991eddd8af67fb89a4
Andrew-0106 <andrewadel0106@gmail.com>
2026-09-08 19:48:55 +0300

    modified:   app.py 	modified:   songs.gitignore 	modified:   static/style.css 	modified:   templates/index.html 	modified:   templates/layout.html 	new file:   templates/player.html 	deleted:    templates/search.html

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request ,send_from_directory
 import sqlite3
 from make_db import make_song_db, dict_factory
 
 app = Flask(__name__)
+
 make_song_db()
+
 
 @app.route("/")
 def index():
@@ -28,12 +30,27 @@ def search():
 
 
  
-@app.route("/Player", methods=["POST"])
-def music_player():
-    song = request.form.get("song", "")
+@app.route("/Player/<int:song_id>")
+def player(song_id):
+    if song_id:
+        with sqlite3.connect("songs.db") as conn:
+            conn.row_factory = dict_factory
+            songs_db = conn.cursor()
+            song = songs_db.execute(
+                "SELECT * FROM songs WHERE id=?",(song_id,)).fetchone()
+        if song:
+            print(song)
+            return render_template("player.html", found= True, song=song)
+        else:
+            return render_template("player.html", found= False)
+    else: 
+        return render_template("player.html", found= False)
+
     
 
-
+@app.route('/songs/<path:filename>')
+def songs(filename):
+    return send_from_directory('songs', filename)
 
 
 
